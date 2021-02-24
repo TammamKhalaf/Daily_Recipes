ec476ea222b47c1c661925042e52f8bbaf07ca2b
Tammam Khalaf <tammam-963@hotmail.com>
2021-02-24 16:00:32 +0400

    theming & styling

--- lib/categories_screen.dart
+++ lib/categories_screen.dart
@@ -1,15 +1,28 @@
 import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+import 'category_item.dart';
+import 'models/dummy_data.dart';
 
 class CategoriesScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return GridView(
-      children: [],
-      gridDelegate: SliverGridDelegateWithMaxCrossAxisExtent(
-        maxCrossAxisExtent: 200,
-        childAspectRatio: 3 / 2,
-        crossAxisSpacing: 20,
-        mainAxisSpacing: 20,
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text("Daily Recipes"),
+      ),
+      body: GridView(
+        padding: const EdgeInsets.all(25),
+        children: DUMMY_CATEGORIES
+            .map((categoryData) =>
+                CategoryItem(categoryData.title, categoryData.color))
+            .toList(),
+        gridDelegate: SliverGridDelegateWithMaxCrossAxisExtent(
+          maxCrossAxisExtent: 200,
+          childAspectRatio: 3 / 2,
+          crossAxisSpacing: 20,
+          mainAxisSpacing: 20,
+        ),
       ),
     );
   }
--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,8 @@
+import 'dart:ui';
+
 import 'package:flutter/material.dart';
+
+import 'categories_screen.dart';
 
 void main() => runApp(MyApp());
 
@@ -8,15 +12,22 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: 'Daily Recipes',
       theme: ThemeData(
-        primarySwatch: Colors.blue,
-      ),
-      home: MyHomePage(),
+          primarySwatch: Colors.pink,
+          accentColor: Colors.amber,
+          canvasColor: Color.fromRGBO(255, 254, 229, 1),
+          fontFamily: 'Raleway',
+          textTheme: ThemeData.light().textTheme.copyWith(
+                body1: TextStyle(color: Color.fromRGBO(20, 51, 51, 1)),
+                body2: TextStyle(color: Color.fromRGBO(20, 51, 51, 1)),
+                title: TextStyle(fontSize: 20, fontFamily: 'RobotoCondensed',fontWeight: FontWeight.bold),
+              )),
+      home: CategoriesScreen(),
     );
   }
 }
 
 class MyHomePage extends StatefulWidget {
-    @override
+  @override
   _MyHomePageState createState() => _MyHomePageState();
 }
 
