df834c22b05ed289fdf53d234914a64739fed468
Tammam Khalaf <tammam-963@hotmail.com>
2021-02-27 22:45:27 +0400

    first version android

--- android/app/src/main/kotlin/com/example/DailyRecipes/MainActivity.kt
+++
@@ -1,6 +0,0 @@
-package com.example.DailyRecipes
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity() {
-}
---
+++ android/app/src/main/kotlin/com/tammamkhalaf/DailyRecipes/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.tammamkhalaf.DailyRecipes
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
